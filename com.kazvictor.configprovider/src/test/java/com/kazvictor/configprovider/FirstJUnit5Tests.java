f259bd8147d28da40276e9133610a49533eb22da
kazvictor <kazvictor@gmail.com>
2019-02-24 10:02:06 -0800

    minor style fixes and updating travis build to use java 11

--- com.kazvictor.configprovider/src/test/java/com/kazvictor/configprovider/FirstJUnit5Tests.java
+++ com.kazvictor.configprovider/src/test/java/com/kazvictor/configprovider/FirstJUnit5Tests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class FirstJUnit5Tests {
+class FirstJUnit5Tests {
     @Test
     void myFirstTest() {
         assertEquals(2, 1 + 1);
